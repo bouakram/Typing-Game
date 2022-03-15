b72ec3b448641b63391768a02a0e70febe4f6746
Akrem Boughazi <kikoua203@gmail.com>
2022-03-15 22:11:23 +0100

    new update

--- js/sctipte.js
+++ js/sctipte.js
@@ -1,8 +1,19 @@
+let replay = 0
+
+var bs=localStorage.getItem("bestscore");
+console.log(bs)
+
 // array of words
-let words = [
-    "the","of","and","a","to","in","is","you","that","it","he","was","for","on","are","as","with","his","they","I","at","be","this","have","from","or","one","had","by","word","but","not","what","all","were","we","when","your","can","said","there","use","an","each","which","she","do","how","their","if","will","up","other","about","out","many","then","them","these","so","some","her","would","make","like","him","into","time","has","look","two","more","write","go","see","number","no","way","could","people","my","than","first","water","been","call","who","oil","its","now","find","long","down","day","did","get","come","made","may","part"
+const wordsE = [
+    "the","me" ,"of","and","a","to","in","is","you","that","it","he","was","for","on","are","as","with","his","they","I","at","be","this","have","from","or","one","had","by","word","but","not","what","all","were","we","when","your","can","said","there","use","an","each","which","she","do","how","their","if","will","up","other","about","out","many","then","them","these","so","some","her","would","make","like","him","into","time","has","look","two","more","write","go","see","number","no","way","could","people","my","than","first","water","been","call","who","oil","its","now","find","long","down","day","did","get","come","made","may","part" 
 ]
 
+const wordsN = ["fanatic",
+"geeks","for","geeks","a",    "portal","to","learn","can",   "be","computer","science", "zoom","yup","fire","in","be","data","geeks","their","if","will","up","other","about","out","many","then","them","these","so","some","her","would","make","like","him","into","time","been","call","who","oil","its","now","find","long","down","day","did","get","come","made","may","part"]
+
+const wordsH = ["fantastics","score","calculate", "landscape", "dare" ,"level", "involvement", "sufficient", "headline", "could", "index" ,"procedure", "them" ,"bread" ,"cow" ,"material","commercial" ,"indeed", "protection", "development", "avoid"]
+
+let words=Array.from(wordsN);
 // setting levels
 
 let levls = {
@@ -13,12 +24,54 @@ let levls = {
 
 // default lvl
 // let defaultLvl = document.querySelector('input[name="def"]:checked').value //change level from here
-let defaultLvl = "Normal"
+
+// start
+// chose ure defuculity level
+var forme = document.getElementById("def");
+var x= document.querySelector('input[name="deffeculity"]:checked');
+
+console.log(x.value); 
+
+forme.onchange= function(){
+    x= document.querySelector('input[name="deffeculity"]:checked');
+    defaultLvl = x.value 
+    lvlNamesSpan.innerHTML = defaultLvl
+    defaultSeconds = levls[defaultLvl]
+    secoundSpan.innerHTML = defaultSeconds
+    timeSpam.innerHTML = defaultSeconds
+
+    // new idea
+    
+    if(defaultLvl === "Easy"){
+        words = Array.from(wordsE)
+    }
+    else if(defaultLvl === "Normal"){
+        words = Array.from(wordsN)
+    }else{
+        words = Array.from(wordsH)
+    }
+}
+
+// asigment the time to 0 whene enter ok
+window.addEventListener("keydown",function(e){
+    
+    if (e.code=="Enter"){
+        
+    timeSpam.innerHTML = "0";
+    
+}
+});
+
+// end
+
+
+let defaultLvl = x.value
 let defaultSeconds = levls[defaultLvl]
 
 // catch selectors
 
 let startBtn = document.querySelector(".start")
+let restartBtn = document.querySelector(".Restart")
 let lvlNamesSpan = document.querySelector(".messages .lvl")
 let secoundSpan = document.querySelector(".messages .seconds")
 let theword = document.querySelector(".the-word")
@@ -27,6 +80,7 @@ let input = document.querySelector(".input")
 let timeSpam = document.querySelector(".time span")
 let scoreGot = document.querySelector(".score .got")
 let scoreTotal = document.querySelector(".score .total")
+let scoreBest =  document.querySelector(".score .bestscore")
 let finishMessage = document.querySelector(".finish")
 
 // setting levls names + seconds + score
@@ -35,7 +89,7 @@ lvlNamesSpan.innerHTML = defaultLvl
 secoundSpan.innerHTML = defaultSeconds
 timeSpam.innerHTML = defaultSeconds
 scoreTotal.innerHTML = words.length
-
+scoreBest.innerHTML = bs
 // disable paste event 
 
 input.onpaste = function (){
@@ -45,10 +99,36 @@ input.onpaste = function (){
 //start game
 
 startBtn.addEventListener("click", function (){
+    forme.style.display="none"
     this.remove()
     input.focus()
     // generate words function 
     generateWords()
+})
+// restart gane 
+restartBtn.style.display = "none"
+restartBtn.addEventListener("click", function(){
+     
+    if(replay === 1){
+        scoreGot.innerHTML="0"
+        forme.style.display="none"
+        restartBtn.style.display = "none"
+        // this.remove()
+        input.focus()
+        replay = 0
+        finishMessage.innerHTML = ''
+        input.value = ''
+        // generate words function 
+        if(defaultLvl === "Easy"){
+            words = Array.from(wordsE)
+        }
+        else if(defaultLvl === "Normal"){
+            words = Array.from(wordsN)
+        }else{
+            words = Array.from(wordsH)
+        }
+        generateWords()
+    }
 })
 
 //function generator words 
@@ -80,7 +160,7 @@ function generateWords(){
 function startplay(){
     timeSpam.innerHTML = defaultSeconds
     let start = setInterval(()=> {
-        timeSpam.innerHTML--
+        
         if(timeSpam.innerHTML === "0"){
             //stop timne
             clearInterval(start)
@@ -93,22 +173,45 @@ function startplay(){
                 if(words.length > 0){//if still words
                     generateWords()
                 }else{
-                    let span = document.createElement('span')
+                    var span = document.createElement('span')
                     span.className = 'good'
-                    let spantext = document.createTextNode('You Win')
+                    var spantext = document.createTextNode('You Win')
                     span.appendChild(spantext)
                     finishMessage.appendChild(span)
                     //remove upcaming box
                     upcomingWord.remove()
+                    timeSpam.innerHTML = "1"
+                    
+                    replay = 1
+                    restartBtn.style.display = "block"
+                    
+                    forme.style.display="block"
+                    if (scoreGot.innerHTML > scoreBest.innerHTML){
+                        
+                        scoreBest.innerHTML=scoreGot.innerHTML
+                        
+                        localStorage.setItem("bestscore",scoreBest.innerHTML );
+                    }
                 }
             }else { // game over 
-                let span = document.createElement("span")
+                var span = document.createElement("span")
                 span.className = 'bad'
-                let spantext = document.createTextNode('Game Over')
+                var spantext = document.createTextNode('Game Over')
                 span.appendChild(spantext)
                 finishMessage.appendChild(span)
+                timeSpam.innerHTML = "1"
+                replay = 1
+                restartBtn.style.display = "block"
+                
+                forme.style.display="block"
+                if (scoreGot.innerHTML > scoreBest.innerHTML){
+                    scoreBest.innerHTML=scoreGot.innerHTML
+                    
+                    localStorage.setItem("bestscore",scoreBest.innerHTML );
+                }
             }
         }
+        timeSpam.innerHTML--
     },1000)
 
 }
